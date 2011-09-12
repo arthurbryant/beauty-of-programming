3e11a2ab6d375b11acb614c5188af63ce3ce4f36
arthurbryant <cst.feng@gmail.com>
2011-09-12 20:34:22 +0800

    add

--- reverseLink/main.c
+++ reverseLink/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
-char * reverse(char *p)
-{
-    char * p1, *p2, *p3;
-
-    p1 = p;
-    if(NULL == p1)
-    {
-        printf("link is null\n");
-        }
-    }
+#include <stdlib.h>
+#include "../../c-test/dataStruct/link.h"
 
 int main(int argc, char * argv[])
 {
+    int arr[] = {1, 2, 3, 4, 5};
+    int len = sizeof(arr) / sizeof(int);
+    List head;
+    head = init();
+    createList(arr, len, head);
+    display(head);
 
-    
+    reverse(head);
+    display(head);
+     
     return 0;
 }
